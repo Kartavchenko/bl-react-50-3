d3b4a495ea4a003d7f195684775eb252059037ad
SeRgNeschetniy <xdsergx@gmail.com>
2022-10-14 21:39:12 +0300

    add detail info country

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,10 +1,30 @@
 import { Section, Container, CountryInfo, Loader } from 'components';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { fetchCountry } from 'service/country-service';
 
 export const Country = () => {
+  const [country, setCountry] = useState([]);
+  const { countryId } = useParams();
+
+  useEffect(() => {
+    const fetchCountryInfo = async () => {
+      const data = await fetchCountry(countryId);
+      setCountry(data);
+      //console.log(data);
+    };
+    fetchCountryInfo();
+  }, [countryId]);
+
+  if (!country) {
+    return;
+  }
+
   return (
     <Section>
       <Container>
-        <h2>Country</h2>
+        <CountryInfo countryInfo={country} />
       </Container>
     </Section>
   );
